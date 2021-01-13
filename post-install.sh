319c498a8cef31e1c4fe8531b421b852c4224770
Ricardowec51 <53792450+Ricardowec51@users.noreply.github.com>
2021-01-13 13:14:06 -0500

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -84,9 +84,10 @@ Do you want to install docker? If so type y / If you dont want to install enter 
 read $docker
 
 if [[ $docker -eq "y" ]] || [[ $docker -eq "yes" ]]; then
+    sudo apt update
     sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
     sudo apt-get update -y
     apt-cache policy docker-ce
     sudo apt install docker-ce -y
