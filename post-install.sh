ba24b7b89822b1c5ba47e2ab4c752e572eef92a2
Ricardowec51 <53792450+Ricardowec51@users.noreply.github.com>
2021-01-18 14:37:11 -0500

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -100,7 +100,7 @@ if [[ $docker -eq "y" ]] || [[ $docker -eq "yes" ]]; then
     "
 
     sudo docker volume create portainer_data
-    sdocker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
+    sudo docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
 
 
     echo "
